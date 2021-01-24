fcaca0bbb3498d6b855776814ee774567ce61d14
spencerNelson-dev <spencernelson144@gmail.com>
2021-01-23 22:41:53 -0700

    moving things around

---
+++ learnYouAHaskell/higherOrderFunc.hs
@@ -0,0 +1,57 @@
+
+-- Currying
+multThree :: (Num a) => a -> a -> a -> a
+multThree x y z = x * y * z
+
+-- higher-order function
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f y x = f x y
+
+largestDivisible :: (Integral a) => a
+largestDivisible = head (filter p [100000,99999..])
+    where p x = x `mod` 3829 == 0
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+    | even n = n:chain (n `div` 2)
+    | odd n = n:chain (n*3 + 1)
+
+numLongChains :: Int 
+numLongChains = length (filter isLong (map chain [1..1000]))
+    where isLong xs = length xs > 15
+
+elem' :: (Eq a) => a -> [a] -> Bool 
+elem' y ys = foldl (\acc x -> if x == y then True else acc) False ys
+
+map' :: (a -> b) -> [a] -> [b]
+map' f xs = foldr (\x acc -> f x : acc) [] xs
+
+maximum' :: (Ord a) => [a] -> a  
+maximum' = foldr1 (\x acc -> if x > acc then x else acc)  
+  
+reverse' :: [a] -> [a]  
+reverse' = foldl (\acc x -> x : acc) []  
+  
+product' :: (Num a) => [a] -> a  
+product' = foldr1 (*)  
+  
+filter' :: (a -> Bool) -> [a] -> [a]  
+filter' p = foldr (\x acc -> if p x then x : acc else acc) []  
+  
+head' :: [a] -> a  
+head' = foldr1 (\x _ -> x)  
+  
+last' :: [a] -> a  
+last' = foldl1 (\_ x -> x)  
+
+sqrtSums :: Int 
+sqrtSums = length (takeWhile (<1000) (scanl1 (+) (map sqrt [1..]))) + 1
---
+++ learnYouAHaskell/recursion.hs
@@ -0,0 +1,49 @@
+
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs)
+    | x > maxTail = x
+    | otherwise = maxTail
+    where maxTail = maximum' xs
+
+maximum'' :: (Ord a) => [a] -> a
+maximum'' [] = error "maximum of empty list"
+maximum'' [x] = x
+maximum'' (x:xs) = max x (maximum'' xs)
+
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+    | n <= 0     = []
+    | otherwise  = x:replicate' (n-1) x
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+    | n <= 0    = []
+take' _ []      = []
+take' n (x:xs)  = x : take' (n-1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' ::a -> [a]
+repeat' x = x:repeat' x
+
+zip' :: [a] -> [b] -> [(a,b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x,y) : zip' xs ys
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (x:xs)
+    | a == x    = True 
+    | otherwise = a `elem'` xs
+
+quickSort :: (Ord a) => [a] -> [a]
+quickSort [] = []
+quickSort (x:xs) =
+    let smallerSorted = quickSort [a | a <- xs, a <= x]
+        biggerSorted = quickSort [ a | a <- xs, a > x]
+    in smallerSorted ++ [x] ++ biggerSorted
